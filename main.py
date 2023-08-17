c11b38ac778a4151f47d7ec536c5829e84ec4346
Stefan Bochow <stefan@janero.com>
2023-08-17 13:22:59 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import json
+import os
+# This is a sample Python script.
+
+# Press Umschalt+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+    cwd = os.getcwd()
+    filename = cwd + '/Job.json'
+
+    if not os.path.exists(filename):
+        print("could not find " + filename)
+    else:
+        print("File " + filename)
+        with open(filename) as f:
+            data = json.loads(f.read())
+            print(data)
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
